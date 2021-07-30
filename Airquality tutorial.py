9ed77f5f909bb90bdd86aee461d4e5dca49c57b6
miner5758 <44759088+miner5758@users.noreply.github.com>
2021-07-30 02:57:07 -0600

    Update Airquality tutorial.py

--- Airquality tutorial.py
+++ Airquality tutorial.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-air_quality = pd.read_csv(r"C:\Users\paula\Downloads\air_quality_no2.csv",index_col=0, parse_dates=True) ##The usage of the index_col and parse_dates parameters of the read_csv function to define the first (0th) column as index of the resulting DataFrame and convert the dates in the column to Timestamp objects, respectively.
+air_quality = pd.read_csv(r"air_quality_no2.csv",index_col=0, parse_dates=True) ##The usage of the index_col and parse_dates parameters of the read_csv function to define the first (0th) column as index of the resulting DataFrame and convert the dates in the column to Timestamp objects, respectively.
 print(air_quality.head())
 #air_quality.plot()
 #air_quality['station_paris'].plot()
