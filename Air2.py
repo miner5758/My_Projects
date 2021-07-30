0e7cbaef08fedb878b907e2c131ecf46f1cebf01
miner5758 <44759088+miner5758@users.noreply.github.com>
2021-07-30 03:07:39 -0600

    Update Air2.py

--- Air2.py
+++ Air2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-air_quality = pd.read_csv(r"C:\Users\paula\Downloads\air_quality_no2_long.csv")
+air_quality = pd.read_csv(r"air_quality_no2_long.csv")
 air_quality = air_quality.rename(columns={"date.utc": "datetime"})
 #print(air_quality.head())
 #print(air_quality.city.unique())
